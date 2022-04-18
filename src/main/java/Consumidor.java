dd14e30920fff6815538ab0a6324c8f3ae9e300f
Diego Frazão <diiegofrazao10@gmail.com>
2022-04-18 20:47:09 -0300

    feat: add atividade

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -3,6 +3,8 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
+import java.nio.charset.StandardCharsets;
+
 
 public class Consumidor {
     public static void main(String[] args) throws Exception {
@@ -11,19 +13,36 @@ public class Consumidor {
         Connection conexao = connectionFactory.newConnection();
         Channel canal = conexao.createChannel();
 
-        String NOME_FILA = "plica"
-                + "";
+        String NOME_FILA = "plica";
         canal.queueDeclare(NOME_FILA, false, false, false, null);
+        System.out.println ("[*] Aguardando mensagens. Para sair, pressione CTRL + C");
 
         DeliverCallback callback = (consumerTag, delivery) -> {
-            String mensagem = new String(delivery.getBody());
-            System.out.println("Eu " + consumerTag + " Recebi: " + mensagem);
+            String mensagem = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println("[-] Mensagem recebida: " + mensagem);
+            try {
+                doWork(mensagem);
+            } finally {
+                System.out.println("[x] Processamento finalizado\n\n");
+                canal.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
 
         // fila, noAck, callback, callback em caso de cancelamento (por exemplo, a fila foi deletada)
         canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {
             System.out.println("Cancelaram a fila: " + NOME_FILA);
         });
+    }
+    private static void doWork(String msg) {
+        for (char ch : msg.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
 
--- src/main/java/Produtor.java
+++ src/main/java/Produtor.java
@@ -1,6 +1,7 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 public class Produtor {
 
@@ -10,15 +11,37 @@ public class Produtor {
                 Connection connection = connectionFactory.newConnection();
                 Channel canal = connection.createChannel();
         ) {
-            String mensagem = "Olá";
+            String mensagem1 = "Mensagem 1 .";
+            String mensagem2 = "Mensagem 2 ..";
+            String mensagem3 = "Mensagem 3 ...";
+            String mensagem4 = "Mensagem 4 ....";
+            String mensagem5 = "Mensagem 5 .....";
+            String mensagem6 = "Mensagem 6 ......";
+            String mensagem7 = "Mensagem 7 .......";
+            String mensagem8 = "DIEGO FRANKNEY FRAZÃO DA SILVA";
             String NOME_FILA = "plica";
 
             //(queue, passive, durable, exclusive, autoDelete, arguments)
             canal.queueDeclare(NOME_FILA, false, false, false, null);
 
             // ​(exchange, routingKey, mandatory, immediate, props, byte[] body)
-            canal.basicPublish("", NOME_FILA, false, false, null, mensagem.getBytes());
+            canal.basicPublish("", NOME_FILA, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem1.getBytes());
+            canal.basicPublish("", NOME_FILA, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem2.getBytes());
+            canal.basicPublish("", NOME_FILA, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem3.getBytes());
+            canal.basicPublish("", NOME_FILA, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem4.getBytes());
+            canal.basicPublish("", NOME_FILA, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem5.getBytes());
+            canal.basicPublish("", NOME_FILA, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem6.getBytes());
+            canal.basicPublish("", NOME_FILA, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem7.getBytes());
+            canal.basicPublish("", NOME_FILA, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem8.getBytes());
 
+            System.out.println ("[x] Enviado: '" + mensagem1 + "'");
+            System.out.println ("[x] Enviado: '" + mensagem2 + "'");
+            System.out.println ("[x] Enviado: '" + mensagem3 + "'");
+            System.out.println ("[x] Enviado: '" + mensagem4 + "'");
+            System.out.println ("[x] Enviado: '" + mensagem5 + "'");
+            System.out.println ("[x] Enviado: '" + mensagem6 + "'");
+            System.out.println ("[x] Enviado: '" + mensagem7 + "'");
+            System.out.println ("[x] Enviado: '" + mensagem8 + "'");
         }
     }
 }
